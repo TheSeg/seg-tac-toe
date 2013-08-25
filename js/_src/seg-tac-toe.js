a0eebd0673ca78c633df910639f372309b441e05
Seg <seg@segonmedia.com>
2013-08-24 18:50:13 -0700

    Adding custom scripts and LESS structure.

---
+++ js/_src/seg-tac-toe.js
@@ -0,0 +1,13 @@
+/*
+
+  Seg-Tac-Toe
+    by John "Seg" Seggerson
+    http://theseg.github.io/
+
+*/
+
+// @codekit-append "stt-logic.js";
+
+$( document ).ready(function() {
+    
+});
---
+++ js/_src/stt-logic.js
@@ -0,0 +1,11 @@
+/*
+
+  Seg-Tac-Toe
+    by John "Seg" Seggerson
+    http://theseg.github.io/
+
+*/
+
+function MainLogic() {
+    console.log("MainLogic();");
+}
